1745a355476e3055346c488dd2527b9814701c56
AndriusRasimas <andrius.rasi@gmail.com>
2021-12-07 23:32:17 +0200

    update

---
+++ classwork/7.2/custom.js
@@ -0,0 +1,10 @@
+function getImages(){
+    return [
+        "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/bycycle.jpg",
+        "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/chess.jpg",
+        "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/jump.jpg",
+        "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/kiting.jpg",
+        "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/lion.jpg",
+        "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/onwater.jpg"
+    ]
+}
